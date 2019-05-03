da59dc5e397ec4359f3821d62419c62c07d1fde7
Nguyễn Đức Mạnh <manh16nd@gmail.com>
2019-05-03 23:42:55 +0700

    Gen User

---
+++ GenAdmin.js
@@ -0,0 +1,21 @@
+const [nodeCommand, fileName, username, password] = process.argv
+const {hashText} = require('./helpers/bcrypt')
+const {Users} = require('./models')
+
+const mongoose = require('mongoose')
+mongoose.set('debug', true)
+mongoose.connect('mongodb://webappdev:k61ca2@ds237196.mlab.com:37196/u-faculties', {useNewUrlParser: true})
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+
+hashText(password)
+    .then(hash => {
+        const user = new Users({
+            username,
+            password: hash,
+            type: 'admin'
+        })
+
+        user.save().then(ok => console.log(ok)).catch(err => console.log(err))
+    })
+    .catch(err => console.log(err))
