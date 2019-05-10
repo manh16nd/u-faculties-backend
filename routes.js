56f324cbc8902213264bc21c44e07733f2d5e5d9
lamngcnguyen <ngoclamc3pro@gmail.com>
2019-05-10 10:23:15 +0700

    Consistent controller filename

--- routes.js
+++ routes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const rootController = require('./controllers/root')
+const rootController = require('./controllers/rootController')
 
 router.get('/', (req, res) => res.send('ok'))
 
@@ -8,18 +8,18 @@ router.post('/', rootController.addAuthor)
 router.patch('/', rootController.changeAuthor)
 router.delete('/', rootController.removeAuthor)
 
-const teacher = require('./controllers/teacher')
+const teacher = require('./controllers/teacherController')
 router.get('/teachers', teacher.getTeachers)
 router.post('/teachers', teacher.addTeacher)
 
-const department = require('./controllers/departments')
+const department = require('./controllers/departmentController')
 router.get('/departments', department.getDepartments)
 router.post('/departments', department.addDepartment)
 
-const fields =  require('./controllers/fields')
+const fields =  require('./controllers/fieldController')
 router.get('/fields', fields.getFields)
 
-const authController = require('./controllers/auth')
+const authController = require('./controllers/authController')
 router.post('/auth/login', authController.login)
 router.post('/auth/addUser', authController.createUser)
 
