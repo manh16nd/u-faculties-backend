3568098e2f2eeae8f8189a4d2206cfd80186ac16
Nguyễn Đức Mạnh <manh16nd@gmail.com>
2019-05-11 11:38:33 +0700

    Error & 404 handle

--- app.js
+++ app.js
@@ -32,20 +32,19 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    next(createError(404))
+app.use((req, res) => {
+    res.status(404).send({
+        success: false,
+        message: 'Route not found',
+    })
 })
 
-// error handler
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message
-    res.locals.error = req.app.get('env') === 'development' ? err : {}
-
-    // render the error page
-    res.status(err.status || 500)
-    res.render('error')
+app.use((err, req, res) => {
+    const message = err.message || err
+    res.send({
+        success: false,
+        message,
+    })
 })
 
 module.exports = app
