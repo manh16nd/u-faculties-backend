61a8419db93d8a30c9c41d98e945594ad17cd891
Lam Nguyen <lamngcnguyen@users.noreply.github.com>
2019-05-13 23:18:27 +0700

    add change password action

--- models/fields.js
+++ models/fields.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const fields = new mongoose.Schema({
     name: String,
     parent: mongoose.Types.ObjectId,
+    teacher: mongoose.Types.ObjectId
 })
 
 module.exports = mongoose.model('fields', fields)
