da980c30cd7c033387a64343ce5c47d5954efcf1
Nguyễn Đức Mạnh <manh16nd@gmail.com>
2019-05-20 21:32:19 +0700

    remove teacher from fields

--- actions/teachers/teacherFieldsActions.js
+++ actions/teachers/teacherFieldsActions.js
@@ -9,13 +9,14 @@ const _validateRemoveFields = (fields, userFields) => {
     return fields.filter((field) => isObjectId(field) && userFields.includes(field))
 }
 
-
 const _addTeacherToField = async (fieldId, teacher) => {
     const field = await Fields.findOne({
         _id: fieldId
     }).select('_id teachers')
-    if (!field) return 123
+
+    if (!field) return null
     if (field.teachers.includes(teacher._id)) return null
+
     field.teachers = [...field.teachers, teacher._id]
     teacher.fields = [...teacher.fields, field._id]
     const teacherField = new TeacherFields({
@@ -30,6 +31,28 @@ const _addTeacherToField = async (fieldId, teacher) => {
     ])
 }
 
+const _removeTeacherFromField = async (fieldId, teacher) => {
+    const field = await Fields.findOne({
+        _id: fieldId
+    }).select('_id teachers')
+
+    if (!field) return null
+    if (!field.teachers.includes(teacher._id)) return null
+
+    field.teachers = field.teachers.filter((item) => item.toString() !== teacher._id.toString())
+    teacher.fields = teacher.fields.filter((item) => item.toString() !== field._id.toString())
+    const teacherField = await TeacherFields.findOne({
+        teacher: teacher._id,
+        field: field._id,
+    })
+
+    return await Promise.all([
+        teacher.save(),
+        field.save(),
+        teacherField ? teacherField.delete() : null,
+    ])
+}
+
 exports.addTeacherToFields = async ({teacherId, fields}) => {
     const teacher = await Teachers.findOne({
         _id: teacherId
@@ -37,13 +60,11 @@ exports.addTeacherToFields = async ({teacherId, fields}) => {
     if (!teacher) throw new Error('Teacher not found')
 
     const validFields = _validateFields(fields, teacher.fields)
-
     const work = validFields.map((field) => {
         return _addTeacherToField(field, teacher)
     })
-    const result = await Promise.all(work)
 
-    return result
+    return await Promise.all(work)
 }
 
 exports.removeTeacherFromFields = async ({teacherId, fields}) => {
@@ -52,10 +73,9 @@ exports.removeTeacherFromFields = async ({teacherId, fields}) => {
     }).select('_id fields')
     if (!teacher) throw new Error('Teacher not found')
     const validFields = _validateRemoveFields(fields, teacher.fields)
+    const work = validFields.map((field) => {
+        return _removeTeacherFromField(field, teacher)
+    })
 
-    return {
-        teacherId,
-        teacher,
-        fields: validFields,
-    }
+    return await Promise.all(work)
 }
